966daec88208213f8e5e1129da7f559a7cd7be93
Jake Walton <jakewalton@Mac-mini.local>
2022-07-11 22:18:00 -0500

    2nd commit: game page working

--- main_app/views.py
+++ main_app/views.py
@@ -1,15 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from django.views.generic import ListView
-from django.views.generic.detail import DetailView
-from django.contrib.auth import login
-from django.contrib.auth.forms import UserCreationForm  
-# from .models import Cat, Toy, Photo
-# from .forms import FeedingForm
-import uuid
-# import boto3
+from .models import Game
 
 def home(request):
     return render(request, 'home.html')
@@ -18,4 +8,14 @@ def about(request):
   return render(request, 'about.html')
 
 def games_index(request):
-    return render(request, 'games/index.html', {'games'})
+    games = Game.objects.all()
+    return render(request, 'games/index.html', {'games': games})
+
+def games_detail(request, game_id):
+    game = Game.objects.get(id=game_id)
+    return render(request, 'games/detail.html', { 'game': game})
+
+class Games:
+    def __init__(self, name, developer):
+        self.name = name
+        self.developer = developer
